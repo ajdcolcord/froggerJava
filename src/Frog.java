25069d4293e8545ceacd43e72627705d9eae68ac
Nick Alekhine <nick.alekhine@gmail.com>
2013-11-25 14:06:15 -0500

    continued rework of Frog.

--- src/Frog.java
+++ src/Frog.java
@@ -2,42 +2,62 @@
  * TABLE OF CONTENTS
  *   1. Libraries
  *   2. Frog
- *      2.1 FIELDS
- *          2.1.1 posn  -- Posn
- *          2.1.2 lives -- int
- *          2.1.3 image -- WorldImage
- *      2.2 METHODS
- *          2.2.1 moveFrogUp()      -- void
- *          2.2.2 moveFrogDown()    -- void
- *          2.2.3 moveFrogLeft()    -- void
- *          2.2.4 moveFrogRight()   -- void
- *          2.2.5 loseLife()        -- void 
+ *    2.1 FIELDS
+ *      2.1.1 posn   -- Posn
+ *      2.1.2 lives  -- int
+ *      2.1.3 image  -- String
+ *      2.1.4 width  -- int
+ *      2.1.5 height -- int
+ *    2.2 CONSTRUCTORS
+ *      2.2.1 Frog()
+ *    2.3 METHODS
+ *      2.3.1 moveFrogUp()       -- void
+ *      2.3.2 moveFrogDown()     -- void
+ *      2.3.3 moveFrogLeft()     -- void
+ *      2.3.4 moveFrogRight()    -- void
+ *      2.3.5 loseLife()         -- void 
+ *      2.3.6 move(Boolean, int) -- void
+ *      2.3.7 makeImage()        -- WorldImage
  *          
  *****************************************************************************/
 
-// 1. Libraries ////////////////////////////////////////////////////////////
+// 1 - Libraries //////////////////////////////////////////////////////////////
 import javalib.colors.*;
 import javalib.funworld.*;
 import javalib.worldcanvas.*;
 import javalib.worldimages.*;
 import java.awt.Color;
 
-///////////////////////////////////////////////////////////////////////////
 
 
-//2. Frog /////////////////////////////////////////////////////////////////
+
+
+//2 - Frog ////////////////////////////////////////////////////////////////////
 /** represents the frog that is controlled by the player 
- * 
  * @author Austin Colcord
+ * @author Nick Alekhine
+ * 
  * */
 public class Frog implements FroggerWorldConstants {
-    // 2.1 //////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+    // 2.1 - Fields ///////////////////////////////////////////////////////////
     Posn posn;
     int lives;
     String image;
     int width;
     int height;
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    ///////////////////////////////////////////////////////////////////////////
+    // 2.2 - Constructors /////////////////////////////////////////////////////
     Frog() {
         this.posn = new Posn((canvasWidth / 2), 475);
         this.lives = 3;
@@ -47,7 +67,18 @@ public class Frog implements FroggerWorldConstants {
 
     }
 
-    // 2.2.1 //////////////////////////////////////////////////////////////////
+    
+    
+    
+    
+    
+    
+    
+    
+    ///////////////////////////////////////////////////////////////////////////
+    // 2.3 - Methods //////////////////////////////////////////////////////////
+    
+    // 2.3.1 - moveFrogUp() ///////////////////////////////////////////////////
     /** move the frog up one step 
      * @author Austin Colcord
      * @author Nick Alekhine
@@ -63,7 +94,11 @@ public class Frog implements FroggerWorldConstants {
         }
     }
 
-    // 2.2.2 //////////////////////////////////////////////////////////////////
+    
+    
+    
+    
+    // 2.3.2 - moveFrogDown() /////////////////////////////////////////////////
     /** move the frog down one step, unless the frog is at the bottom
      * row, then do nothing
      * @author Austin Colcord
@@ -80,7 +115,11 @@ public class Frog implements FroggerWorldConstants {
         }
     }
 
-    // 2.2.3 //////////////////////////////////////////////////////////////////
+    
+    
+    
+    
+    // 2.3.3 - moveFrogLeft() /////////////////////////////////////////////////
     /** move the frog to the left, unless near the left edge of the canvas 
      * @author Austin Colcord
      * @author Nick Alekhine
@@ -96,7 +135,11 @@ public class Frog implements FroggerWorldConstants {
         }
     }
 
-    // 2.2.4 ////////////////////////////////////////////////////
+    
+    
+    
+    
+    // 2.3.4 - moveFrogRight() ////////////////////////////////////////////////
     /**  move the frog to the right unless near the right edge of the canvas 
      * @author Austin Colcord
      * @author Nick Alekhine
@@ -112,7 +155,11 @@ public class Frog implements FroggerWorldConstants {
         }
     }
     
-    // 2.2.5 ////////////////////////////////////////////////////
+    
+    
+    
+    
+    // 2.3.5 - loseLife() ////////////////////////////////////////////////////.
     /** make the frog lose a life by 1. this will be called if
      * the frog falls in the water or is hit by a car
      * @author Austin Colcord
@@ -121,12 +168,33 @@ public class Frog implements FroggerWorldConstants {
         this.lives = this.lives - 1;
     }
     
-    // 2.2.6 //////////////////////////////////////////////////////////////////
+    
+    
+    
+    
+    // 2.3.6 - move(Boolean, int) /////////////////////////////////////////////
     public void move(Boolean facingLeft, int speed) {
-        
+        // if moving left
+        if (facingLeft) {
+            // if x coordinate is not beyond canvas
+            if (this.posn.x < (canvasWidth / 20)) {
+                this.posn.x = this.posn.x - speed;
+            }
+        }
+        // else moving right
+        else {
+            // if x coordinate is not beyond canvas
+            if (this.posn.x < (canvasWidth - (canvasWidth / 20))) {
+                this.posn.x = this.posn.x + speed;
+            }
+        }
     }
     
     
+    
+    
+    
+    // 2.3.7 - makeImage() ////////////////////////////////////////////////////
     public WorldImage makeImage() {
         return new FromFileImage(this.posn, this.image);
     }
