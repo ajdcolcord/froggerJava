8b048163fc0e51cecd2eaa296bdd26a175dc5fce
Nick Alekhine <nick.alekhine@gmail.com>
2013-11-29 22:02:57 -0600

    update gamerunner.

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,29 +1,3 @@
-/******************************************************************************
- * TABLE OF CONTENTS
- *   1. Libraries
- *   2. GameRunner
- *     2.1 FIELDS
- *       2.1.1 fw -- FroggerWorld
- *     2.2 CONSTRUCTORS
- *       2.2.1 GameRunner()
- *       2.2.2 GameRunner(FroggerWorld)
- *     2.3 METHODS
- *       2.3.1 onTick()                     -- void
- *       2.3.2 onKeyEvent(String)           -- void
- *       2.3.3 worldEnds()                  -- WorldEnd
- *       2.3.4 makeImage()                  -- WorldImage
- *       2.3.5 lastImage(String)            -- WorldImage
- * 
- *****************************************************************************/
-
-
-
-
-
-
-
-
-// 2 - GamerRunner ////////////////////////////////////////////////////////////
 /** the class that runs the Yeezus Game
  * 
  * @author Austin
@@ -40,15 +14,16 @@ public class GameRunner {
     /** the initial world that invokes the <code>bigBang</code> method */
     YeezusWorld yw = this.ey.initYeezusWorld();
 
-    /** 
-     * Method that runs the game
+    
+    
+    
+    /**  Method that runs the game
      */
     void run() {
         this.yw.bigBang(1000, 500, 0.075);
     }
 
-    /**
-     * Main method to run the game
+    /** Main method to run the game
      * @param argv unused
      */
     public static void main(String[] argv) {
